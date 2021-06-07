1590d9bac5601aaca51eb4f63829e1661c4e98c6
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2021-06-07 12:11:58 +0300

    Update bully.js

--- commands/fun/bully.js
+++ commands/fun/bully.js
@@ -13,8 +13,8 @@ module.exports = {
 		var dau_bully = bully.bullys[Math.floor(Math.random() * bully.bullys.length)];
         let victima = message.mentions.members.first();
 	console.log(victima.user.id);
-	if(victima.user.id = 422408402849890304) return console.log("E HaZZe");
-	if(victima.user.id = 251087166900404225) return console.log("E Giusepe");
+	if(victima.user.id == 422408402849890304) return console.log("E HaZZe");
+	else if(victima.user.id == 251087166900404225) return console.log("E Giusepe");
         if(!victima) return message.channel.send("Nu pot să dau bully la nimic...");
 		message.channel.send( "<@" + victima.user.id + ">" + " " + dau_bully);
     }
