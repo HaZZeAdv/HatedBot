435c7b848678c73634cdd0caab231f3eced42d76
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2022-11-13 11:01:34 +0200

    Update ping.js

--- commands/miscellaneous/ping.js
+++ commands/miscellaneous/ping.js
@@ -1,19 +1,23 @@
-module.exports = { 
-    config: {
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	config: {
         name: "ping",
         description: "PONG! Displays the api & bot latency",
         usage: ``,
         category: "miscellaneous",
         accessableby: "Members"
     },
-    run: async (bot, message, args) => {
-
-    message.channel.send("Pinging...").then(m => {
-        let ping = m.createdTimestamp - message.createdTimestamp
-        let choices = ["Is this really my ping", "Is it okay? I cant look", "I hope it isnt bad"]
-        let response = choices[Math.floor(Math.random() * choices.length)]
-
-        m.edit(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
-    })
-  }
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Show ping of the server'),
+	async execute(bot, message) {
+		message.channel.send("Pinging...").then(m => {
+			let ping = m.createdTimestamp - message.createdTimestamp
+			let choices = ["Is this really my ping", "Is it okay? I cant look", "I hope it isnt bad"]
+			let response = choices[Math.floor(Math.random() * choices.length)]
+	
+			m.edit(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
+		})
+	}
 }
